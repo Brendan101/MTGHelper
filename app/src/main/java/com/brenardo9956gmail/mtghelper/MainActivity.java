0ea19772f19601d51b01e9eac838905a32f1f886
Bren101 <b101@umbc.edu>
2017-12-19 14:55:56 -0500

    UX - Screen always on

--- app/src/main/java/com/brenardo9956gmail/mtghelper/MainActivity.java
+++ app/src/main/java/com/brenardo9956gmail/mtghelper/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         
         //player one 
         playerOneInc = (Button)findViewById(R.id.playerOneInc);
